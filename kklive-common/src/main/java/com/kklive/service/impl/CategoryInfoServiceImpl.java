2b6e82cc1a60b9895aba6a0d0408fd399b4aeb3a
lonelykkk <2765314967@qq.com>
2025-01-17 20:54:11 +0800

    分类管理02-多级分类、分类上下移排序

--- kklive-common/src/main/java/com/kklive/service/impl/CategoryInfoServiceImpl.java
+++ kklive-common/src/main/java/com/kklive/service/impl/CategoryInfoServiceImpl.java
@@ -30,15 +30,11 @@ public class CategoryInfoServiceImpl implements CategoryInfoService {
     @Resource
     private CategoryInfoMapper<CategoryInfo, CategoryInfoQuery> categoryInfoMapper;
 
-    @Resource
-    private VideoInfoService videoInfoService;
 
     @Resource
     private RedisComponent redisComponent;
 
-    /**
-     * 根据条件查询列表
-     */
+
     @Override
     public List<CategoryInfo> findListByParam(CategoryInfoQuery param) {
         List<CategoryInfo> categoryInfoList = this.categoryInfoMapper.selectList(param);
@@ -48,134 +44,69 @@ public class CategoryInfoServiceImpl implements CategoryInfoService {
         return categoryInfoList;
     }
 
-    private List<CategoryInfo> convertLine2Tree(List<CategoryInfo> dataList, Integer pid) {
-        List<CategoryInfo> children = new ArrayList();
-        for (CategoryInfo m : dataList) {
-            if (m.getCategoryId() != null && m.getpCategoryId() != null && m.getpCategoryId().equals(pid)) {
-                m.setChildren(convertLine2Tree(dataList, m.getCategoryId()));
-                children.add(m);
-            }
-        }
-        return children;
-    }
-
-    /**
-     * 根据条件查询列表
-     */
     @Override
     public Integer findCountByParam(CategoryInfoQuery param) {
-        return this.categoryInfoMapper.selectCount(param);
+        return null;
     }
 
-    /**
-     * 分页查询方法
-     */
     @Override
     public PaginationResultVO<CategoryInfo> findListByPage(CategoryInfoQuery param) {
-        int count = this.findCountByParam(param);
-        int pageSize = param.getPageSize() == null ? PageSize.SIZE15.getSize() : param.getPageSize();
-
-        SimplePage page = new SimplePage(param.getPageNo(), count, pageSize);
-        param.setSimplePage(page);
-        List<CategoryInfo> list = this.findListByParam(param);
-        PaginationResultVO<CategoryInfo> result = new PaginationResultVO(count, page.getPageSize(), page.getPageNo(), page.getPageTotal(), list);
-        return result;
+        return null;
     }
 
-    /**
-     * 新增
-     */
     @Override
     public Integer add(CategoryInfo bean) {
-        return this.categoryInfoMapper.insert(bean);
+        return null;
     }
 
-    /**
-     * 批量新增
-     */
     @Override
     public Integer addBatch(List<CategoryInfo> listBean) {
-        if (listBean == null || listBean.isEmpty()) {
-            return 0;
-        }
-        return this.categoryInfoMapper.insertBatch(listBean);
+        return null;
     }
 
-    /**
-     * 批量新增或者修改
-     */
     @Override
     public Integer addOrUpdateBatch(List<CategoryInfo> listBean) {
-        if (listBean == null || listBean.isEmpty()) {
-            return 0;
-        }
-        return this.categoryInfoMapper.insertOrUpdateBatch(listBean);
+        return null;
     }
 
-    /**
-     * 多条件更新
-     */
     @Override
     public Integer updateByParam(CategoryInfo bean, CategoryInfoQuery param) {
-        StringTools.checkParam(param);
-        return this.categoryInfoMapper.updateByParam(bean, param);
+        return null;
     }
 
-    /**
-     * 多条件删除
-     */
     @Override
     public Integer deleteByParam(CategoryInfoQuery param) {
-        StringTools.checkParam(param);
-        return this.categoryInfoMapper.deleteByParam(param);
+        return null;
     }
 
-    /**
-     * 根据CategoryId获取对象
-     */
     @Override
     public CategoryInfo getCategoryInfoByCategoryId(Integer categoryId) {
-        return this.categoryInfoMapper.selectByCategoryId(categoryId);
+        return null;
     }
 
-    /**
-     * 根据CategoryId修改
-     */
     @Override
     public Integer updateCategoryInfoByCategoryId(CategoryInfo bean, Integer categoryId) {
-        return this.categoryInfoMapper.updateByCategoryId(bean, categoryId);
+        return null;
     }
 
-    /**
-     * 根据CategoryId删除
-     */
     @Override
     public Integer deleteCategoryInfoByCategoryId(Integer categoryId) {
-        return this.categoryInfoMapper.deleteByCategoryId(categoryId);
+        return null;
     }
 
-    /**
-     * 根据CategoryCode获取对象
-     */
     @Override
     public CategoryInfo getCategoryInfoByCategoryCode(String categoryCode) {
-        return this.categoryInfoMapper.selectByCategoryCode(categoryCode);
+        return null;
     }
 
-    /**
-     * 根据CategoryCode修改
-     */
     @Override
     public Integer updateCategoryInfoByCategoryCode(CategoryInfo bean, String categoryCode) {
-        return this.categoryInfoMapper.updateByCategoryCode(bean, categoryCode);
+        return null;
     }
 
-    /**
-     * 根据CategoryCode删除
-     */
     @Override
     public Integer deleteCategoryInfoByCategoryCode(String categoryCode) {
-        return this.categoryInfoMapper.deleteByCategoryCode(categoryCode);
+        return null;
     }
 
     @Override
@@ -195,14 +126,24 @@ public class CategoryInfoServiceImpl implements CategoryInfoService {
         save2Redis();
     }
 
+    private void save2Redis() {
+        CategoryInfoQuery categoryInfoQuery = new CategoryInfoQuery();
+        categoryInfoQuery.setOrderBy("sort asc");
+        List<CategoryInfo> sourceCategoryInfoList = this.categoryInfoMapper.selectList(categoryInfoQuery);
+        List<CategoryInfo> categoryInfoList = convertLine2Tree(sourceCategoryInfoList, 0);
+        redisComponent.saveCategoryList(categoryInfoList);
+    }
+
+
     @Override
     public void delCategory(Integer categoryId) {
-        VideoInfoQuery videoInfoQuery = new VideoInfoQuery();
+        // todo 查询分类下是否有视频
+        /*VideoInfoQuery videoInfoQuery = new VideoInfoQuery();
         videoInfoQuery.setCategoryIdOrPCategoryId(categoryId);
         Integer count = videoInfoService.findCountByParam(videoInfoQuery);
         if (count > 0) {
             throw new BusinessException("分类下有视频信息，无法删除");
-        }
+        }*/
 
         CategoryInfoQuery categoryInfoQuery = new CategoryInfoQuery();
         categoryInfoQuery.setCategoryIdOrPCategoryId(categoryId);
@@ -212,12 +153,11 @@ public class CategoryInfoServiceImpl implements CategoryInfoService {
         save2Redis();
     }
 
-
     @Override
     public void changeSort(Integer pCategoryId, String categoryIds) {
         String[] categoryIdArray = categoryIds.split(",");
         List<CategoryInfo> categoryInfoList = new ArrayList<>();
-        Integer sort = 1;
+        Integer sort = 0;
         for (String categoryId : categoryIdArray) {
             CategoryInfo categoryInfo = new CategoryInfo();
             categoryInfo.setCategoryId(Integer.parseInt(categoryId));
@@ -230,21 +170,19 @@ public class CategoryInfoServiceImpl implements CategoryInfoService {
         save2Redis();
     }
 
-    private void save2Redis() {
-        CategoryInfoQuery categoryInfoQuery = new CategoryInfoQuery();
-        categoryInfoQuery.setOrderBy("sort asc");
-        List<CategoryInfo> sourceCategoryInfoList = this.categoryInfoMapper.selectList(categoryInfoQuery);
-        List<CategoryInfo> categoryInfoList = convertLine2Tree(sourceCategoryInfoList, 0);
-        redisComponent.saveCategoryList(categoryInfoList);
-    }
-
-
     @Override
     public List<CategoryInfo> getAllCategoryList() {
-        List<CategoryInfo> categoryInfoList = redisComponent.getCategoryList();
-        if (categoryInfoList.isEmpty()) {
-            save2Redis();
+        return null;
+    }
+
+    private List<CategoryInfo> convertLine2Tree(List<CategoryInfo> dataList, Integer pid) {
+        List<CategoryInfo> children = new ArrayList();
+        for (CategoryInfo m : dataList) {
+            if (m.getCategoryId() != null && m.getpCategoryId() != null && m.getpCategoryId().equals(pid)) {
+                m.setChildren(convertLine2Tree(dataList, m.getCategoryId()));
+                children.add(m);
+            }
         }
-        return redisComponent.getCategoryList();
+        return children;
     }
 }
