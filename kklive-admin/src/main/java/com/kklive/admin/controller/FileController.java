a10d8208a1c329396af608fde036b6b33200241f
lonelykkk <2765314967@qq.com>
2025-01-17 21:32:43 +0800

    分类管理03-文件上传

---
+++ kklive-admin/src/main/java/com/kklive/admin/controller/FileController.java
@@ -0,0 +1,108 @@
+package com.kklive.admin.controller;
+
+import com.kklive.entity.config.AppConfig;
+import com.kklive.entity.constants.Constants;
+import com.kklive.entity.enums.DateTimePatternEnum;
+import com.kklive.entity.enums.ResponseCodeEnum;
+import com.kklive.entity.vo.ResponseVO;
+import com.kklive.exception.BusinessException;
+import com.kklive.utils.DateUtil;
+import com.kklive.utils.FFmpegUtils;
+import com.kklive.utils.StringTools;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Date;
+
+import static com.kklive.admin.controller.ABaseController.STATUC_SUCCESS;
+
+/**
+ * @author lonelykkk
+ * @email 2765314967@qq.com
+ * @date 2025/1/17 20:57
+ * @Version V1.0
+ */
+@Validated
+@Slf4j
+@RestController
+@RequestMapping("/file")
+public class FileController {
+    @Resource
+    private AppConfig appConfig;
+    @Resource
+    private FFmpegUtils fFmpegUtils;
+
+    /**
+     * 文件上传
+     * @param file 文件
+     * @param createThumbnail 是否生成缩略图
+     * @return
+     */
+    @RequestMapping("/uploadImage")
+    public ResponseVO uploadImage(@NotNull MultipartFile file, @NotNull Boolean createThumbnail) throws IOException {
+        String month = DateUtil.format(new Date(), DateTimePatternEnum.YYYYMM.getPattern());
+        String folder = appConfig.getProjectFolder() + Constants.FILE_FOLDER + Constants.FILE_COVER + month;
+        File folderFile = new File(folder);
+        if (!folderFile.exists()) {
+            folderFile.mkdirs();
+        }
+        String fileName = file.getOriginalFilename(); //文件原始名，从此获取文件后缀并重新命名
+        String fileSuffix = fileName.substring(fileName.lastIndexOf("."));  // 文件后缀
+        String realFileName = StringTools.getRandomString(Constants.LENGTH_30) + fileSuffix; // 文件重命名
+        String filePath = folder + "/" + realFileName;
+        file.transferTo(new File(filePath));
+        if (createThumbnail) {
+            //生成缩略图
+            fFmpegUtils.createImageThumbnail(filePath);
+        }
+        return getSuccessResponseVO(Constants.FILE_COVER + month + "/" + realFileName);
+    }
+
+    @RequestMapping("/getResource")
+    public void getResource(HttpServletResponse response, @NotEmpty String sourceName) {
+        if (!StringTools.pathIsOk(sourceName)) {
+            throw new BusinessException(ResponseCodeEnum.CODE_600);
+        }
+        String suffix = StringTools.getFileSuffix(sourceName);
+        response.setContentType("image/" + suffix.replace(".", ""));
+        response.setHeader("Cache-Control", "max-age=2592000");
+        readFile(response, sourceName);
+    }
+
+    protected <T> ResponseVO getSuccessResponseVO(T t) {
+        ResponseVO<T> responseVO = new ResponseVO<>();
+        responseVO.setStatus(STATUC_SUCCESS);
+        responseVO.setCode(ResponseCodeEnum.CODE_200.getCode());
+        responseVO.setInfo(ResponseCodeEnum.CODE_200.getMsg());
+        responseVO.setData(t);
+        return responseVO;
+    }
+
+    protected void readFile(HttpServletResponse response, String filePath) {
+        File file = new File(appConfig.getProjectFolder() + Constants.FILE_FOLDER + filePath);
+        if (!file.exists()) {
+            return;
+        }
+        try (OutputStream out = response.getOutputStream(); FileInputStream in = new FileInputStream(file)) {
+            byte[] byteData = new byte[1024];
+            int len = 0;
+            while ((len = in.read(byteData)) != -1) {
+                out.write(byteData, 0, len);
+            }
+            out.flush();
+        } catch (Exception e) {
+            log.error("读取文件异常", e);
+        }
+    }
+}
