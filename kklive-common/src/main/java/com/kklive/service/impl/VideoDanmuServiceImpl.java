a6b0b913ffa0af6026565b050290afad1b1c220a
lonelykkk <2765314967@qq.com>
2025-01-30 18:35:21 +0800

    创作中心02-管理视频相关信息

--- kklive-common/src/main/java/com/kklive/service/impl/VideoDanmuServiceImpl.java
+++ kklive-common/src/main/java/com/kklive/service/impl/VideoDanmuServiceImpl.java
@@ -42,12 +42,19 @@ public class VideoDanmuServiceImpl implements VideoDanmuService {
 
     @Override
     public Integer findCountByParam(VideoDanmuQuery param) {
-        return null;
+        return this.videoDanmuMapper.selectCount(param);
     }
 
     @Override
     public PaginationResultVO<VideoDanmu> findListByPage(VideoDanmuQuery param) {
-        return null;
+        int count = this.findCountByParam(param);
+        int pageSize = param.getPageSize() == null ? PageSize.SIZE15.getSize() : param.getPageSize();
+
+        SimplePage page = new SimplePage(param.getPageNo(), count, pageSize);
+        param.setSimplePage(page);
+        List<VideoDanmu> list = this.findListByParam(param);
+        PaginationResultVO<VideoDanmu> result = new PaginationResultVO(count, page.getPageSize(), page.getPageNo(), page.getPageTotal(), list);
+        return result;
     }
 
     @Override
@@ -107,8 +114,25 @@ public class VideoDanmuServiceImpl implements VideoDanmuService {
         // TODO 跟新es 弹幕数量
     }
 
+    /**
+     * 删除弹幕
+     * @param userId
+     * @param danmuId
+     */
     @Override
     public void deleteDanmu(String userId, Integer danmuId) {
+        VideoDanmu danmu = videoDanmuMapper.selectByDanmuId(danmuId);
+        if (null == danmu) {
+            throw new BusinessException(ResponseCodeEnum.CODE_600);
+        }
+        VideoInfo videoInfo = videoInfoMapper.selectByVideoId(danmu.getVideoId());
+        if (null == videoInfo) {
+            throw new BusinessException(ResponseCodeEnum.CODE_600);
+        }
 
+        if (userId != null && !videoInfo.getUserId().equals(userId)) {
+            throw new BusinessException(ResponseCodeEnum.CODE_600);
+        }
+        videoDanmuMapper.deleteByDanmuId(danmuId);
     }
 }
